8cc7c604f44323daaf79d720d90cccc79b28b2c9
jiweixiaoxiao <4309473@qq.com>
2017-10-16 19:17:56 +0800

    新闻页面

--- ocPortal/src/test/com/online/college/service/QiNiuTest.java
+++ ocPortal/src/test/com/online/college/service/QiNiuTest.java
@@ -16,13 +16,13 @@ public class QiNiuTest extends TestCase {
 
 	public void testImages() {
 		//测试上传图片
-		byte[] buff = CommonUtil.getFileBytes(new File("D://jiwei1.png"));
-		String key = QiniuStorage.uploadImage(buff);
-		System.out.println("key = " + key);
+		//byte[] buff = CommonUtil.getFileBytes(new File("D://2222.mp4"));
+		//String key = QiniuStorage.uploadImage(buff);
+		//System.out.println("key = " + key);
 		
 	//	String key = "/default/all/0/0b75266553d24506a1df015f2b52d2f8.jpeg";
 		//测试下载图片
-		String url = QiniuStorage.getUrl(key);
+		String url = QiniuStorage.getUrl("uizhivedio");
 		System.out.println("url = " + url);
 
 		//测试下载不同大小的图片
